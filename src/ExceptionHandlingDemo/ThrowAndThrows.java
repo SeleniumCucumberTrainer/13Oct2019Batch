39a5a3352966ac4febd0e5f0ff02f5ab486ac1ea
akhilesg <akhilesg@gmail.com>
2019-12-21 22:00:33 +0530

    exception handling

--- src/ExceptionHandlingDemo/ThrowAndThrows.java
+++ src/ExceptionHandlingDemo/ThrowAndThrows.java
@@ -1,18 +1,33 @@
 package ExceptionHandlingDemo;
 
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+
 public class ThrowAndThrows {
 	
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws NullPointerException, FileNotFoundException {
 		
-		int age =18;
+		int age =81;
 		
-		if(age>18){
+		if(age>=18 && age<60){
 			
 			System.out.println("Eligble for voting");
 			
 		}
-		else if(age>60){
+		else if(age>80){
+			
+			
+				new FileInputStream("File path");
+			
+			
+			
+			System.out.println("Not Eligble for voting");
+		}
+		
+		else {
+			
+			throw new NullPointerException("Hi Throw Demo");
 			
 		}
 		
